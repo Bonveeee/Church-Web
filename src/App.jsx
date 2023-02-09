de328d49a2343b11ba4a880df45fd0729a63fd0b
bonveeee <bonochieng@gmail.com>
2023-02-09 11:41:42 +0300

    [added]starter routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./components/Home";
+import Kids from "./components/Kids";
+import Sermon from "./components/Sermon";
+import Staff from "./components/Staff";
+import PlanVisit from "./components/PlanVisit";
+import About from "./components/About";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1 className="text-green-600 text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route  path="/" exact element ={<Home/>} />
+        <Route path="/kids" element={<Kids/>} />
+        <Route path="/sermon" element={<Sermon/>} />
+        <Route path="/staff" element={<Staff/>} />
+        <Route path="/planvisit" element={<PlanVisit/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <h1 className="text-3xl font-bold underline">
+     Welcome Home!
+    </h1>
+  );
+};
 
-export default Home
+export default Home;
