74a00a964194f8979c0e8eb4294b57782e944298
bonveeee <bonochieng@gmail.com>
2023-02-12 22:48:57 +0300

    commit

--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -1,7 +1,20 @@
 
 const Staff = () => {
   return (
-    <div><h1>Meet our Staff</h1></div>
+    <div><h1>Meet our Staff</h1>
+    <div>
+      <h1>Clergy</h1>
+    </div>
+    <div>
+      <h1>Sunday School</h1>
+    </div>
+    <div>
+      <h1>Youths</h1>
+    </div>
+    <div>
+      
+    </div>
+    </div>
   )
 }
 
