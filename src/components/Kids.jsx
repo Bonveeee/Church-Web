338edf4dcf73354f7bb5f2c5e158d13551b542e3
bonveeee <bonochieng@gmail.com>
2023-02-13 22:03:13 +0300

    kids page update

--- src/components/Kids.jsx
+++ src/components/Kids.jsx
@@ -1,7 +1,20 @@
 
 const Kids = () => {
   return (
-    <div>Kids</div>
+    <div className="flex flex-col items-center p-10">
+      <h1 className="text-2xl font-bold mb-5">Welcome to the Children's Ministry Page!</h1>
+      <img
+        src="https://via.placeholder.com/300x300"
+        alt="Fun illustration for kids"
+        className="w-64 mb-5"
+      />
+      <p className="text-lg font-medium">
+        Join us as we learn about God's love and have fun together!
+      </p>
+      <button className="bg-indigo-500 text-white py-2 px-4 rounded mt-5">
+        Learn More
+      </button>
+    </div>
   )
 }
 
