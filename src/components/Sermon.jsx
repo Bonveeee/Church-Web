307edc049f8fc676aebb612f9765d6e1b95eecb2
bonveeee <bonochieng@gmail.com>
2023-02-15 21:30:19 +0300

    sermon page

--- src/components/Sermon.jsx
+++ src/components/Sermon.jsx
@@ -1,10 +1,17 @@
 
 const Sermon = () => {
   return (
-    <div>
-    Recent Sermons
-    <div>Video</div>
+    <div className="container mx-auto my-8">
+    <h2 className="text-2xl font-bold mb-4">Latest Sermon</h2>
+    <div className="relative h-0" style={{ paddingBottom: "56.25%" }}>
+      <iframe
+        className="absolute inset-0 w-full h-full"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        title="Latest Sermon"
+        allowFullScreen
+      />
     </div>
+  </div>
   )
 }
 
