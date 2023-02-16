7bfdd6e63c7ddbfe129ceadfdad0525e86102618
bonveeee <bonochieng@gmail.com>
2023-02-16 22:33:59 +0300

    sermon update

--- src/components/Sermon.jsx
+++ src/components/Sermon.jsx
@@ -7,9 +7,13 @@ const Sermon = () => {
       <iframe
         className="absolute inset-0 w-full h-full"
         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          
         title="Latest Sermon"
         allowFullScreen
       />
+       <button className="bg-indigo-500 text-white py-2 px-4 rounded mt-5">
+        More
+      </button>
     </div>
   </div>
   )
