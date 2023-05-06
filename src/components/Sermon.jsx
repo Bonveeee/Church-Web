ba9215bff7e44a5879116ccf2a9df5a0d2b9f003
Bonveee <bonochieng@gmail.com>
2023-05-06 16:59:12 +0300

    [update]sermon page fetch youtube appi

--- src/components/Sermon.jsx
+++ src/components/Sermon.jsx
@@ -1,22 +1,48 @@
+import React, { useState, useEffect } from "react";
 
 const Sermon = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const apiKey = "AIzaSyBrSO2tBHjHRyFfyYiBKq2vbQGwtMw4RPc";
+    const playlistId = "PLB4IZODwmK5mvt418XpgF96nhic5t6Pws";
+    const maxResults = 6;
+    fetch(
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=${maxResults}&order=date&type=video&key=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data.items);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="container mx-auto my-8">
-    <h2 className="text-2xl font-bold mb-4">Latest Sermon</h2>
-    <div className="relative h-0" style={{ paddingBottom: "56.25%" }}>
-      <iframe
-        className="absolute inset-0 w-full h-full"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          
-        title="Latest Sermon"
-        allowFullScreen
-      />
-       <button className="bg-indigo-500 text-white py-2 px-4 rounded mt-5">
-        More
-      </button>
+      <h2 className="text-2xl font-bold mb-7 text-center">Latest Sermon</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {videos.map((video) => (
+          <a
+            key={video.id}
+            href={`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative h-0" style={{ paddingBottom: "56.25%" }}>
+              <img
+                className="absolute inset-0 w-full h-full object-cover"
+                src={video.snippet.thumbnails.medium.url}
+                alt={video.snippet.title}
+              />
+            </div>
+            <div className="mt-2 text-gray-800">{video.snippet.title}</div>
+          </a>
+        ))}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Sermon
+export default Sermon;
